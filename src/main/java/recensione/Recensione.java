5069d90bec605edc7843fb703b323a974849bdf6
Francesco <francescodidom00@outlook.it>
2023-01-02 21:21:12 +0100

    -Fix: Modifica nome variabile da commento a testo in Recensione.java -Feat: Aggiunto metodo doRetrieveAll in RecensioneDAO che restituisce tutte le recensioni all interno del DB. -Feat: Aggiunto metodo doSave in RecensioneDAO che permette di inserire nuove recensioni all interno del DB. -Feat: Aggiunto metodo doDelete in RecensioneDAO che permette di eliminare tramite id una recensione all'interno del DB. -Feat: Aggiunto metodo doUpdate in RecensioneDAO che permette di modificare una recensione presente all'interno del DB. -Feat: Aggiunto metodo getRecensioneById in RecensioneDAO che restituisce tramite id  una recensione all'interno del DB.

--- src/main/java/recensione/Recensione.java
+++ src/main/java/recensione/Recensione.java
@@ -3,11 +3,37 @@ package recensione;
 public class Recensione {
     private int idRecensione;
     private int valutazione;
-    private String commento;
-    public Recensione(int idRecensione,int valutazione,String commento){
+    private String testo;
+    public Recensione(int idRecensione,int valutazione,String testo){
         this.idRecensione=idRecensione;
         this.valutazione=valutazione;
-        this.commento=commento;
+        this.testo = testo;
     }
 
+    public Recensione() {
+    }
+
+    public int getIdRecensione() {
+        return idRecensione;
+    }
+
+    public void setIdRecensione(int idRecensione) {
+        this.idRecensione = idRecensione;
+    }
+
+    public int getValutazione() {
+        return valutazione;
+    }
+
+    public void setValutazione(int valutazione) {
+        this.valutazione = valutazione;
+    }
+
+    public String getTesto() {
+        return testo;
+    }
+
+    public void setTesto(String testo) {
+        this.testo = testo;
+    }
 }
--- src/main/java/storage/RecensioneDAO.java
+++ src/main/java/storage/RecensioneDAO.java
@@ -1,4 +1,90 @@
 package storage;
 
+import recensione.Recensione;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class RecensioneDAO {
-}
+    public ArrayList<Recensione> doRetrieveAll(){//Metodo che permette di restituire tutte le recensioni presenti nel DB
+        try (Connection con=ConPool.getConnection()){
+            PreparedStatement ps=con.prepareStatement("SELECT * FROM Recensione");
+            ArrayList<Recensione> recensioni=new ArrayList<>();
+            ResultSet rs=ps.executeQuery();
+            while (rs.next()){
+                Recensione r=new Recensione();
+                r.setIdRecensione(rs.getInt(1));
+                r.setValutazione(rs.getInt(2));
+                r.setTesto(rs.getString(3));
+
+                recensioni.add(r);
+            }
+            con.close();
+            return recensioni;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void doUpdate(int idRecensione,int valutazione,String testo){
+        try(Connection con=ConPool.getConnection()) {
+            PreparedStatement ps = con.prepareStatement("UPDATE Recensione set valutazione=?,testo=? where idRecensione=?;");
+            ps.setInt(1, valutazione);
+            ps.setString(2, testo);
+            ps.setInt(3, idRecensione);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void doDelete(int idRecensione){//Metodo che permette di eliminare nel DB una carta tramite id
+        try (Connection con=ConPool.getConnection()){
+            PreparedStatement ps=con.prepareStatement("DELETE FROM Recensione where idRecensione=?");
+            ps.setInt(1,idRecensione);
+            if (ps.executeUpdate() != 1) {
+                throw new RuntimeException("Errore durante la cancellazione della recensione");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Recensione getRecensioneById(int idRecensione) { //Metodo che permette di trovare e restituire una recensione nel DB  tramite id
+        try (Connection con = ConPool.getConnection()) {
+            PreparedStatement ps = con.prepareStatement("SELECT*FROM Recensione WHERE idRecensione=?");
+            ps.setInt(1, idRecensione);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                Recensione r = new Recensione();
+                r.setIdRecensione(rs.getInt(1));
+                r.setValutazione(rs.getInt(2));
+                r.setTesto(rs.getString(3));
+                return r;
+            }
+            con.close();
+            return null;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void doSave(Recensione r){ //Metodo che permette inserire una recensione nel DB
+        try (Connection con=ConPool.getConnection()){
+            PreparedStatement ps=con.prepareStatement("INSERT into Recensione(valutazione,testo)VALUES(?,?);");
+            ps.setInt(1,r.getValutazione());
+            ps.setString(2,r.getTesto());
+            if(ps.executeUpdate()!=1){
+                throw new RuntimeException("Errore durante l'inserimento della recensione");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+    }

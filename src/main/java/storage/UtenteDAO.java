2ebe50a9dc920f9f0062e71ee38dd500a0d9c2db
Francesco <francescodidom00@outlook.it>
2023-01-04 23:50:07 +0100

    -Fix: Aggiustati alcuni metodi che restituiscono un booleano e non un oggetto Utente

--- src/main/java/storage/UtenteDAO.java
+++ src/main/java/storage/UtenteDAO.java
@@ -9,7 +9,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class UtenteDAO {
-    public ArrayList<Utente> getAllUtenti(){//Metodo che permette di trovare e restituire dal DB tutti gli utenti presenti
+    //Metodo che permette di trovare e restituire dal DB tutti gli utenti presenti
+    public ArrayList<Utente> getAllUtenti(){
         try(Connection con=ConPool.getConnection()){
             PreparedStatement ps=con.prepareStatement("SELECT * FROM Utente");
             ArrayList<Utente> utenti=new ArrayList<>();
@@ -32,29 +33,19 @@ public class UtenteDAO {
         }
     }
 
-    public Utente getUtenteByEmail(String email){ ////Metodo che serve per controllo lato server: vede se esiste già un utente all interno del db con stessa email.
-        try (Connection con= ConPool.getConnection()){
-            PreparedStatement ps=con.prepareStatement("SELECT * FROM Utente WHERE email=? ");
-            ps.setString(1,email);
-            ResultSet rs= ps.executeQuery();
-
-            if(rs.next()){
-                Utente u = new Utente();
-                u.setIdUtente(rs.getInt(1));
-                u.setUsername(rs.getString(2));
-                u.setPassword(rs.getString(3));
-                u.setNome(rs.getString(4));
-                u.setCognome(rs.getString(5));
-                u.setEmail(rs.getString(6));
-                return u;
-            }
+    //Serve per controllo lato server: VERIFICA se esiste già un utente all interno del db con stessa email.
+    public boolean getUtenteByEmail(String email) {
+        try (Connection con = ConPool.getConnection()) {
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM Utente WHERE email=? ");
+            ps.setString(1, email);
+            ResultSet rs = ps.executeQuery();
+            return rs.next();
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
-        return null;
     }
-
-    public Utente getUtenteByEmailPassword(String email,String password){//Metodo che permette di trovare un utente nel DB tramite email e password
+    //Metodo che permette di trovare un utente nel DB tramite email e password
+    public Utente getUtenteByEmailPassword(String email,String password){
         try (Connection con=ConPool.getConnection()){
             PreparedStatement ps=con.prepareStatement("SELECT * FROM Utente where email=? AND passwordhash=?");
             ps.setString(1,email);
@@ -78,8 +69,8 @@ public class UtenteDAO {
         }
 
     }
-
-    public Utente getUtenteById(int idUtente){//Metodo che permette di trovare un utente nel DB tramite id  e lo restituisce
+    //Metodo che permette di trovare un utente nel DB tramite id  e lo restituisce
+    public Utente getUtenteById(int idUtente){
         try (Connection con=ConPool.getConnection()){
             PreparedStatement ps=con.prepareStatement("SELECT * FROM Utente WHERE idUtente=?");
             ps.setInt(1,idUtente);
@@ -100,8 +91,8 @@ public class UtenteDAO {
             throw new RuntimeException(e);
         }
     }
-
-    public void doDelete(int idUtente) {//Metodo che permette di eliminare un utente dal DB tramite id
+    //Metodo che permette di eliminare un utente dal DB tramite id
+    public void doDelete(int idUtente) {
         try(Connection con=ConPool.getConnection()){
             PreparedStatement ps= con.prepareStatement("DELETE FROM Utente WHERE idUtente=?");
             ps.setInt(1,idUtente);
@@ -113,8 +104,8 @@ public class UtenteDAO {
         }
 
     }
-
-    public void doSave(Utente u){//Metodo che permette di inserire un utente nel DB (registrazione)
+    //Metodo che permette di inserire un utente nel DB (registrazione)
+    public void doSave(Utente u){
         try (Connection con=ConPool.getConnection()){
             PreparedStatement ps=con.prepareStatement("INSERT into Utente(username,passwordhash,nome,cognome,email) VALUES (?,?,?,?,?);");
             ps.setString(1,u.getUsername());
@@ -130,8 +121,8 @@ public class UtenteDAO {
             throw new RuntimeException(e);
         }
     }
-
-    public void doUpdate(int idUtente,String username,String passwordhash,String nome,String cognome,String email){ //Metodo che permette di modificare i dati di un utente
+    //Metodo che permette di modificare i dati di un utente
+    public void doUpdate(int idUtente,String username,String passwordhash,String nome,String cognome,String email){
         try(Connection con=ConPool.getConnection()){
             PreparedStatement ps=con.prepareStatement("UPDATE Utente set username=?,passwordhash=?,nome=?,cognome=?,email=? where idUtente=?;");
             ps.setString(1,username);
@@ -145,6 +136,17 @@ public class UtenteDAO {
             throw new RuntimeException(e);
         }
 
+    }
+    //Metodo che permette di sapere se un utente è presente nel DB tramite username
+    public boolean getUtenteByUsername(String username) {
+        try (Connection con = ConPool.getConnection()) {
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM Utente WHERE username=? ");
+            ps.setString(1, username);
+            ResultSet rs = ps.executeQuery();
+            return rs.next();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 

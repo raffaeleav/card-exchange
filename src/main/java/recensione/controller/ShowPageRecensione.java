a2c2a9cd4e3bc86c0b9b7a6bb4ec47fc91517b08
Francesco <francescodidom00@outlook.it>
2023-01-06 12:51:30 +0100

    -Feat:creata servlet che rimanda alla jsp della recensione

---
+++ src/main/java/recensione/controller/ShowPageRecensione.java
@@ -0,0 +1,33 @@
+package recensione.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * La classe permette l'indirizzamento verso la pagina di compilazione
+ * form della recensione.
+ * @author Francesco Di Domenico
+ */
+
+@WebServlet("/showPageRecensione")
+public class ShowPageRecensione extends HttpServlet {
+
+    /**
+     * Il metodo permette di gestire la richiesta del client,dove il server
+     * rimanda ad un'altra
+     * pagina a fine tramite parametro RequestDispatcher .
+     * @param req : oggetto di richiesta HTTP
+     * @param resp : oggetto di risposta HTTP
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher dispatcher=req.getRequestDispatcher("recensione.jsp");
+        dispatcher.forward(req,resp);
+    }
+}

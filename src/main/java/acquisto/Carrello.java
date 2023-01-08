c2c3745b3b6d54b8aac30e419b6cfbfcd6824991
michelemenzione <menzione.m@gmail.com>
2023-01-08 21:06:08 +0100

    fix: fix Carrello

--- src/main/java/acquisto/Carrello.java
+++ src/main/java/acquisto/Carrello.java
@@ -39,8 +39,7 @@ public class Carrello {
 
         // Recupera le offerte presenti nel carrello dal database
         // utilizzando il metodo getOfferte del DAO OffertaDAO
-        List<Offerta> offerte = OffertaDAO.getOfferteByIdUtente(this.idUtente);
-        return offerte;
+        return OffertaDAO.getOfferteByIdUtente(this.idUtente);
     }
 
     public void setOfferte(List<Offerta> offerte) {

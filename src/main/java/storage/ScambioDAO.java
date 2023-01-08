a78b4f52b3fb2a15b5945d96bc31b153dc165235
michelemenzione <menzione.m@gmail.com>
2023-01-08 12:17:58 +0100

    Feat: Aggiunto JavaDOC relativo alla classe

--- src/main/java/storage/ScambioDAO.java
+++ src/main/java/storage/ScambioDAO.java
@@ -1,5 +1,6 @@
 package storage;
 
+import registrazione.Utente;
 import scambio.Scambio;
 
 import java.sql.Connection;
@@ -9,8 +10,45 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ScambioDAO {
+    /**
+     * @param idRichiestaScambio - ID della richiesta di scambio da eliminare
+     */
+    public void doDelete(int idRichiestaScambio) {
+        try(Connection con=ConPool.getConnection()){
+            PreparedStatement ps= con.prepareStatement("DELETE FROM RichiestaDiScambio WHERE idRichiestaScambio=?");
+            ps.setInt(1,idRichiestaScambio);
+            if(ps.executeUpdate()!=1){
+                throw new RuntimeException("Errore durante la cancellazione dello scambio");
+            }
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    /**
+     * @param richiestaDiScambio - Lo scambio che verrà memorizzato all'interno del DB
+     */
+    public void doSave(Scambio richiestaDiScambio){
+        try (Connection con=ConPool.getConnection()){
+            PreparedStatement ps=con.prepareStatement("INSERT into RichiestaDiScambio(idUtenteMittente,idUtenteDestinatario,idOfferta) VALUES (?,?,?,?,?);");
+            ps.setInt(1,richiestaDiScambio.getIdUtenteMittente());
+            ps.setInt(2,richiestaDiScambio.getIdUtenteDestinatario());
+            ps.setInt(3,richiestaDiScambio.getIdOfferta());
+
+            if(ps.executeUpdate()!=1){
+                throw new RuntimeException("Errore durante l'inserimento dello scambio");
+            }
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
 
 
+    /**
+     * @param idOfferta - id dell'offerta messa in vendita
+     * @return una lista con tutte le proposte di scambio ricevute per l'offerta
+     */
     // Permette di recuperare tutte le richieste di scambio per una determinata offerta
     public ArrayList<Scambio> getAllScambiByIdOfferta(int idOfferta){
         try(Connection con = ConPool.getConnection()){
@@ -40,6 +78,10 @@ public class ScambioDAO {
         }
     }
 
+    /**
+     * @param idUtenteMittente - ID dell'utente che ha inviato le richieste di scambio
+     * @return Tutte le richieste di scambio effettuate
+     */
     // Permette di recuperare tutte le richieste scambio effettuate da un utente
     public ArrayList<Scambio> getAllScambiByIdMittente(int idUtenteMittente){
         try(Connection con = ConPool.getConnection()){
@@ -69,6 +111,10 @@ public class ScambioDAO {
         }
     }
 
+    /**
+     * @param idUtenteDestinatario - ID dell'utente Destinatario
+     * @return tutte le richieste di scambio ricevute
+     */
     // Permette di recuperare tutte le richieste scambio ricevute da un utente
     public ArrayList<Scambio> getAllScambiByIdDestinatario(int idUtenteDestinatario){
         try(Connection con = ConPool.getConnection()){

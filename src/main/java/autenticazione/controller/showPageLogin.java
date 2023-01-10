c5a79aa4fe8862eb83b9f82720e09f5f4bac6c4f
Francesco <francescodidom00@outlook.it>
2023-01-10 21:58:47 +0100

    -Fix: aggiunti javadoc

--- src/main/java/autenticazione/controller/showPageLogin.java
+++ src/main/java/autenticazione/controller/showPageLogin.java
@@ -9,10 +9,22 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+/**
+ * La classe permette l'indirizzamento verso la pagina di compilazione
+ * form della login.
+ * @author Francesco Di Domenico
+ */
+
 @WebServlet("/mostraPaginaLogin")
 public class showPageLogin extends HttpServlet {
 
-
+    /**
+     * Il metodo permette di gestire la richiesta del client,dove il server
+     * rimanda ad un'altra
+     * pagina a fine tramite parametro RequestDispatcher .
+     * @param req : oggetto di richiesta HTTP
+     * @param resp : oggetto di risposta HTTP
+     */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/results/login.jsp");
--- src/main/java/autenticazione/controller/showPageUtente.java
+++ src/main/java/autenticazione/controller/showPageUtente.java
@@ -8,12 +8,21 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-
+/**
+ * La classe permette l'indirizzamento verso la pagina dell'utente.
+ * @author Francesco Di Domenico
+ */
 
 @WebServlet("/mostraPaginaUtente")
     public class showPageUtente extends HttpServlet {
 
-
+    /**
+     * Il metodo permette di gestire la richiesta del client,dove il server
+     * rimanda ad un'altra
+     * pagina a fine tramite parametro RequestDispatcher .
+     * @param req : oggetto di richiesta HTTP
+     * @param resp : oggetto di risposta HTTP
+     */
         @Override
         protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
             RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/results/pagina-utente.jsp");

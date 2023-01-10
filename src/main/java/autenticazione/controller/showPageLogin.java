60326919fd28bda91c20ec3d2f0a13b95b35af7d
Francesco <francescodidom00@outlook.it>
2023-01-10 20:56:42 +0100

    -Fix: cambio nome servlet

--- src/main/java/autenticazione/controller/showPageLogin.java
+++ src/main/java/autenticazione/controller/showPageLogin.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 @WebServlet("/mostraPaginaLogin")
 public class showPageLogin extends HttpServlet {
-   
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {

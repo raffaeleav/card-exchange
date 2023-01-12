2106c96bd87c8cf3aecac618bd56ec980b5f45ae
raffaeleav <raff.aviello@gmail.com>
2023-01-12 04:46:20 +0100

    chore: aggiunta javadoc

--- src/main/java/partecipazioneDiscussione/control/EliminaMessaggioServlet.java
+++ src/main/java/partecipazioneDiscussione/control/EliminaMessaggioServlet.java
@@ -11,9 +11,22 @@ import storage.FacadeDAO;
 
 import java.io.IOException;
 
+/**
+ * La classe permette l'eliminazione di un messaggio in una discussione
+ * tramite una servlet che viene richiamata dal bottone della
+ * funzione di eliminazione di un messaggio
+ * @author Raffaele Aviello
+ */
+
 @WebServlet("/elimina-messaggio-servlet")
 public class EliminaMessaggioServlet extends HttpServlet {
 
+    /**
+     * Il metodo permette di gestire la richiesta del client tramite una response che
+     * permette l'eliminazione di un messaggio in una discussione tramite la classe FacadeDAO
+     * @param request oggetto che modella una richiesta HTTP
+     * @param response oggetto che modella una risposta HTTP
+     * */
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int messageId = Integer.parseInt(request.getParameter("messagge-id"));

d4f91efdec9151c057e86b9f53cd10df873e6129
Francesco <francescodidom00@outlook.it>
2023-01-15 16:25:00 +0100

    -Feat:se l utente che elimina la recensione è un admin reindirizzalo alla pagina del pannello admin,altrimenti un utente normale viene reindirizzato alla sua pagina utente.

--- src/main/java/recensione/controller/EliminaRecensioneServlet.java
+++ src/main/java/recensione/controller/EliminaRecensioneServlet.java
@@ -5,7 +5,9 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import recensione.service.Recensione;
+import registrazione.Utente;
 import storage.service.FacadeDAO;
 
 import java.io.IOException;
@@ -27,10 +29,13 @@ public class EliminaRecensioneServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        HttpSession session= req.getSession();
+        Utente utente = (Utente) session.getAttribute("Utente");
         int idRecensione= Integer.parseInt(req.getParameter("idRecensione"));
         FacadeDAO facadeDAO=new FacadeDAO();
         facadeDAO.doDelete(Recensione.class,idRecensione);//Elimina recensione dal DB in base al tipo di classe passata e idRecensione.
-        resp.sendRedirect("MostraPannelloAdmin");//reindirizza alla pagina pannello admin
+
+        if(utente.getIdUtente()==1){resp.sendRedirect("MostraPannelloAdmin");}//reindirizza alla pagina pannello admin
+        if(utente.getIdUtente()>=2){req.getRequestDispatcher("/WEB-INF/results/paginaUtente.jsp");}
     }
 }

64d60bbcff17f2dac600450cfae8a7c9e813a774
raffaeleav <raff.aviello@gmail.com>
2023-01-15 19:20:45 +0100

    fix: typos nei file sql

--- src/main/webapp/database/popola-db.sql
+++ src/main/webapp/database/popola-db.sql
@@ -4,12 +4,6 @@ insert into Utente values(default, 'Admin','admin123!','Francesco','Di Domenico'
 insert into Utente values(default, 'utente','utente123!','Francesco','Di Domenico','utente@email.it');
 insert into Utente values(default, 'Admin','admin123!','Francesco','Di Domenico','admin@email.it');
 
-insert into Carta values(default, 'Drago bianco occhi blu', 'Yu-Gi-Oh', 'Comune', '/imgs/cards/yugioh/yugioh.png');
-insert into Carta values(default, 'Mago nero', 'Yu-Gi-Oh', 'Rara', '/imgs/cards/yugioh/yugioh.png');
-insert into Carta values(default, 'Pikachu', 'Pokemon', 'Ultra rara', '/imgs/cards/pokemon/pokemon.png');
-insert into Carta values(default, 'Maga nera', 'Yu-Gi-Oh', 'Comune', '/imgs/cards/yugioh/yugioh.png');
-insert into Carta values(default, 'Loto nero', 'Magic', 'Comune', '/imgs/cards/yugioh/magic.png');
-
 insert into Discussione values(default, "1", "Benvenuti sul forum!");
 insert into Discussione values(default, "2", "Grazie di aver...");
 
@@ -17,5 +11,3 @@ insert into Messaggio values(default, "Benvenuti!", "Vi do il benvenuto sul foru
 insert into Messaggio values(default, "Card eXchange!", "Sentitevi liberi di contattare il nostro supporto per qualsiasi problema.", 1, 1);
 
 insert into Ordine values(default,'2017-06-15','via roma 15',1,55.20);
-
-INSERT INTO Offerta (condizione, prezzo, idUtente, idCarta) VALUES ('Nuova', 5, 1, 4);
